6ef7f79891d978aaffd1b481dbd10ed3a577946c
David Danier <danier@team23.de>
2022-08-24 14:26:48 +0200

    next try

--- sub_pre_commit/cli.py
+++ sub_pre_commit/cli.py
@@ -1,9 +1,14 @@
+from typing import List, Optional
+from pathlib import Path
+
 import sys
 
 import typer
 
 
-def main(name: str = "world"):
+def main(
+    files: Optional[List[Path]] = typer.Option(None),
+):
     print(f"Hello {name}")
     print(sys.argv)
 
