59a846454b770789506e8078b6844b417b5386ea
David Danier <danier@team23.de>
2022-08-24 14:34:28 +0200

    next try

--- sub_pre_commit/cli.py
+++ sub_pre_commit/cli.py
@@ -2,16 +2,30 @@ from typing import List, Optional
 from pathlib import Path
 
 import sys
+import os
 
 import typer
 
 
 def main(
-    files: Optional[List[Path]] = typer.Option(None),
+    path: Path = typer.Option(
+        ...,
+        "--path", "-p",
+        help="Path of the sub pre-commit call."
+    ),
+    files: Optional[List[Path]] = typer.Argument(
+        None,
+        help="List of files to pass to sub pre-commit.",
+    ),
 ):
-    print(f"Hello {name}")
-    print(sys.argv)
+    from pprint import pprint
+    pprint(files)
+    pprint(os.environ)
+
+
+def run():
+    typer.run(main)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    run()
