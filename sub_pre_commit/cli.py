de7cd8612c905ab84a20be0b840bda6e477b7321
David Danier <danier@team23.de>
2022-08-24 14:41:48 +0200

    next try

--- sub_pre_commit/cli.py
+++ sub_pre_commit/cli.py
@@ -18,9 +18,17 @@ def main(
         help="List of files to pass to sub pre-commit.",
     ),
 ):
-    for file in files:
-        if file.is_relative_to(path):
-            print(file)
+    relevant_files = []
+    if files:
+        for file in files:
+            if file.is_relative_to(path):
+                relevant_files.append(file)
+        if not relevant_files:
+            print("Nothing to check, existing")
+            return
+    
+    print(relevant_files)
+    
 
 
 def run():
