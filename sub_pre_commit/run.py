0142ea79e3698020a392290d219eac6e4f9c3fba
David Danier <danier@team23.de>
2022-08-30 08:57:35 +0200

    fix: Use return code from pre-commit as real return code - raise SystemExit like pre-commit itself does

--- sub_pre_commit/run.py
+++ sub_pre_commit/run.py
@@ -5,7 +5,7 @@ def run() -> None:
     # Disable munching the args so pre-commit not only runs in the git root directory
     main._adjust_args_and_chdir = lambda args: None
     # Actually run pre-commit
-    main.main()
+    raise SystemExit(main.main())
 
 
 if __name__ == "__main__":
